1985b7366afdde5de6d4a2ca7aab1f19a9ca5657
vtrieu <vanna.trieu@gmail.com>
2019-11-25 16:41:23 -0500

    Add files via upload

---
+++ validate.py
@@ -0,0 +1,16 @@
+import snowflake.connector
+
+# Gets the version
+ctx = snowflake.connector.connect(
+    user='<your_user_name>',
+    password='<your_password>',
+    account='<your_account_name>'
+    )
+cs = ctx.cursor()
+try:
+    cs.execute("SELECT current_version()")
+    one_row = cs.fetchone()
+    print(one_row[0])
+finally:
+    cs.close()
+ctx.close()
